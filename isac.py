404682354bf4aec0e87a6bdb4986b6c7ef45cfa4
vincent <nattybear@naver.com>
2017-08-16 15:10:00 +0900

    원하는 테이블을 번호로 입력 받아 파싱하는 함수 추가

--- isac.py
+++ isac.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from bs4 import BeautifulSoup
 from sys import argv
 import re
 
@@ -14,3 +15,16 @@ p = re.compile('<td>(\d{4}[.]\d{2}[.]\d{2})</td>')
 m = p.search(b)
 # date 전역 변수에 등록일을 저장
 date = m.group(1)
+
+# html 파싱을 위해 BeautifulSoup 객체를 생성
+s = BeautifulSoup(b, 'html.parser')
+
+# 입력으로 테이블 번호를 받는다.
+# tbody 태그를 모두 찾은 뒤에
+# 입력 받은 번호에 해당하는 테이블 중에서
+# tr 태그만 모아서 리스트로 반환한다.
+def parse(num):
+	a = s.find_all('tbody')
+	b = a[int(num)]
+	c = b.find_all('tr')
+	return c
