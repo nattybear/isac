4e61d656cc188e02b5664e30fedfc326c21c3750
vincent <nattybear@naver.com>
2017-08-16 16:32:59 +0900

    테이블과 칼럼 번호를 입력 받아 원하는 칼럼의 문자열만 반환하는 함수 추가\nparse 함수 이름을 tr로 변경함

--- isac.py
+++ isac.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from sqlite3 import connect
 from bs4 import BeautifulSoup
 from sys import argv
 import re
@@ -23,8 +24,24 @@ s = BeautifulSoup(b, 'html.parser')
 # tbody 태그를 모두 찾은 뒤에
 # 입력 받은 번호에 해당하는 테이블 중에서
 # tr 태그만 모아서 리스트로 반환한다.
-def parse(num):
+def tr(num):
 	a = s.find_all('tbody')
 	b = a[int(num)]
 	c = b.find_all('tr')
-	return c
+	# 테이블 헤더는 제외하고 리턴
+	return c[1:]
+
+# 첫번째 인자로 tr 태그의 리스트를 받는다.
+# 두번째 인자로 출력할 칼럼 번호를 받는다.
+# 이 함수는 두개의 칼럼만 출력 할 수 있다.
+# 입력된 리스트 중에 span 태그만 모아서
+# 하나의 리스트로 반환한다.
+def span(_list, _tuple):
+	# 전체 결과를 담을 임시 리스트를 생성
+	t = []
+	# 파싱 할 칼럼 번호를 지정
+	a, b = _tuple
+	for i in _list:
+		span = i.find_all('span')
+		t.append((span[a].string, span[b].string))
+	return t
