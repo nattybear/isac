5831e485f6f6df9d8fdd0e80c3eb7e7d5b9de9e0
vincent <nattybear@naver.com>
2017-08-23 15:31:42 +0900

    아이피에 대한 국가코드를 조회하는 모듈을 개발
    KISA에서 제공하는 Whois API를 이용해서 아이피를 입력하면 국가코드를 출력하는 함수를 작성. KISA에서 발급받은 키값이 하드코딩 되어 있어서 해당 함수를 커밋할지 말지 고민을 했는데, 그냥 커밋하기로 결정. 내 git을 누가 본다고...

---
+++ whois.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from urllib.request import urlopen
+from sys import argv
+from json import loads
+
+def get(ip):
+	# %s를 이용해서 url 폼을 만든다.
+	urlform = 'http://whois.kisa.or.kr/openapi/ipascc.jsp?query=%s&key=%s&answer=json'
+	# 발급받은 키는 외부에 공개하면 곤란하다.
+	# 따라서 설정 파일을 따로 두고 키값을 읽어올까 생각을 해봤는데
+	# 파일 포인터를 열고 닫는데에 시간이 걸리기 때문에
+	# 좋은 방법은 아닌 것 같다.
+	key = '2016071413065956652873'
+	# % 연산자를 이용해서 url 폼에 아이피와 키를 넣는다.
+	url = urlform % (ip, key)
+	u = urlopen(url)
+	# url 객체는 데이터를 바이트 객체로 받아오기 때문에
+	# decode 함수를 이용해서 utf-8로 적절히 디코딩 해준다.
+	b = u.read().decode('utf-8')
+	# url 객체를 닫아준다.
+	# url 객체도 열고 닫을 때마다 시간이 소요 될텐데
+	# 여러개의 아이피를 한번에 쿼리 할 수 있는 API가 아쉽다.
+	u.close()
+	# json 모듈의 loads 함수를 이용하면
+	# json 문자열 객체를 파이썬 딕셔너리 객체로 변환 할 수 있다.
+	j = loads(b)
+	# 국가 코드를 리턴한다.
+	return j['whois']['countryCode']
+
+if __name__ == '__main__':
+	get(argv[1])
