5942337747c65ccbbd99d47181acc236682670cc
vincent <nattybear@naver.com>
2017-08-31 17:54:41 +0900

    테이블 정의 수정
    테이블 정의를 또 수정했다. 앞으로도 몇번은 더 수정 할 것이다. 이 바로 전의 커밋 메세지는 실수로 잘못 올렸다.

--- blacklist.sql
+++ blacklist.sql
@@ -1,33 +1,24 @@
-BEGIN TRANSACTION;
-
-CREATE TABLE IF NOT EXISTS ip (
-	ip TEXT PRIMARY KEY
+begin transaction;
+create table ip (
+	ip text primary key
 );
 
-CREATE TABLE IF NOT EXISTS category (
-	categoryid INTEGER PRIMARY KEY AUTOINCREMENT,
-	categoryname TEXT UNIQUE,
-	priority INTEGER
+create table attacktype (
+	attacktypeid integer primary key autoincrement,
+	attacktypename text unique,
+	typeid integer,
+	foreign key(typeid) references type(typeid)
 );
 
-INSERT INTO category(categoryname, priority) VALUES ("정보수집", 100);
-INSERT INTO category(categoryname, priority) VALUES ("서비스거부", 100);
-INSERT INTO category(categoryname, priority) VALUES ("악성코드", 50);
-INSERT INTO category(categoryname, priority) VALUES ("피싱", 100);
-INSERT INTO category(categoryname, priority) VALUES ("웹공격", 100);
-
-CREATE TABLE IF NOT EXISTS subcategory (
-	subcategoryid INTEGER PRIMARY KEY AUTOINCREMENT,
-	subcategoryname TEXT UNIQUE,
-	categoryid INTEGER,
-	FOREIGN KEY(categoryid) REFERENCES category(categoryid)
+create table type (
+	typeid integer primary key autoincrement,
+	typename text unique,
+	levelid integer,
+	foreign key(levelid) references level(levelid)
 );
-
-CREATE TABLE IF NOT EXISTS ip_subcategory (
-	ip TEXT,
-	subcategoryid INTEGER,
-	FOREIGN KEY(ip) REFERENCES ip(ip),
-	FOREIGN KEY(subcategoryid) REFERENCES subcategory(subcategoryid)
+create table level (
+	levelid integer primary key autoincrement,
+	levelname text unique,
+	priority integer
 );
-
-COMMIT;
+rollback;
