05c5800dc186185ff1c8b0460aae6efb4ee69910
vincent <nattybear@naver.com>
2017-09-05 00:10:18 +0900

    워드에서 모든 아이피를 담은 리스트를 리턴
    테이블 번호와 칼럼 번호를 이용해서 모든 아이피 주소를 파싱하여 리스트로 반환한다.

--- cti.py
+++ cti.py
@@ -6,13 +6,20 @@ from sys import argv
 
 doc = Document(argv[1])
 
-index = []
-
 # "IP Address"라는 문자열이 있는 테이블 번호와
 # 칼럼의 번호를 파악하고 리스트에 튜플로 저장한다.
+ipindex = []
 for i, table in enumerate(doc.tables):
 	for j, head in enumerate(table.rows[0].cells):
 		if "IP Address" in head.text:
-			index.append((i, j))
+			ipindex.append((i, j))
 
-print(index)
+# 위에서 조사한 테이블 번호와 칼럼 번호를 이용해서
+# 문서의 모든 아이피 주소를 담은 리스트를 만든다.
+iplist = []
+for table, col in ipindex:
+	cells = doc.tables[table].columns[col].cells
+	for cell in cells:
+		if "IP Address" not in cell.text:
+			iplist.append((cell.text,))
+
