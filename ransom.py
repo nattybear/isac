b3cca8000e4d34224644a8e9335f651bb934fb9d
vincent <nattybear@naver.com>
2017-09-07 10:35:48 +0900

    시간 계산 로직 추가. 쿼리 추가. 아이피 구분 함수 추가.
    오늘을 기준으로 며칠 전 데이터를 가져와야 하기 때문에 datetime 모듈을 이용해서 시간을 계산함. 데이터베이스에서 데이터를 가져오기 위해 앞에서 만든 시간 데이터를 이용해서 쿼리를 작성함. 호스트가 아이피인지 도메인인지에 따라 출력을 달리 해야 하기 때문에 커스텀 함수 제작.

--- ransom.py
+++ ransom.py
@@ -2,6 +2,8 @@
 
 from sys import argv
 from sqlite3 import connect, IntegrityError, ProgrammingError
+from datetime import date, timedelta
+from re import compile
 
 # 큰 따옴표를 제거하는 함수
 def delquo(text): return text.replace('"', '')
@@ -9,6 +11,12 @@ def delquo(text): return text.replace('"', '')
 # '","'를 구분으로 문자열을 쪼개는 함수
 def splitcom(text): return text.split('","')
 
+# 입력된 값이 아이피인지 아닌지 알려주는 함수
+def isip(text):
+	p = compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+	m = p.search(text)
+	return True if m else False
+	
 def main():
 	# 데이터베이스에 연결한다.
 	con = connect('blacklist.db')
@@ -31,14 +39,37 @@ def main():
 		if i[0] != '#':
 			t = list(map(delquo, splitcom(i)))
 			try:
-				cur.execute(sql, t)
+				#cur.execute(sql, t)
+				pass
+			# 중복 에러, 외래키 에러
 			except IntegrityError as e:
 				print(e, t)
 				continue
+			# 인코딩 에러
 			except ProgrammingError as e:
 				print(e, t)
 				continue
-	
+
+	# 엑셀 취합 형태로 출력하기
+	# 이틀 전 날짜를 구한다.
+	ago = date.today() - timedelta(days=3)
+	# 쿼리를 작성한다.
+	sql = ''' 
+		SELECT host, malware, threat
+		FROM ransom
+		WHERE firstseen > "%s";''' % ago
+	# 쿼리를 실행한다.
+	cur.execute(sql)
+	# 결과를 받아온다.
+	rows = cur.fetchall()
+	for row in rows:
+		host = row[0]
+		malware = row[1]
+		threat = row[2]
+		docu = "%s ransomware %s" % (malware, threat)
+		t = (host, "도메인", docu, "ransomware tracker")
+		print(t)
+
 	# 데이터베이스 연결을 끊는다.
 	con.commit()
 	con.close()
