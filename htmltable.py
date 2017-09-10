72ece84e0bcb7928800b905b5e9e018ac1dd5a62
vincent <nattybear@naver.com>
2017-09-10 21:51:41 +0900

    테이블 행 개수 찾는 코드 추가
    원본 테이블을 처음부터 파싱해서 원하는 것을 뽑아내려고 하지 말자. 우선은 원본 테이블을 임시 테이블로 작성하고 SQL을 이용해서 다시 저장하는 방식이 어떨지 시험해보자. 여기서 테이블이라는 용어가 겹쳤는데 처음에 말한 테이블은 HTML 표이고 후자는 Sqlite3 테이블이다.

--- htmltable.py
+++ htmltable.py
@@ -3,11 +3,18 @@
 from sys import argv
 from bs4 import BeautifulSoup
 
+# ISAC 파일 인코딩은 UTF-16 리틀 엔디안이다.
 f = open(argv[1], encoding='utf-16-le')
 b = f.read()
 f.close()
 
+# 뷰티풀 스프 객체를 만든다.
 s = BeautifulSoup(b, 'html.parser')
-tables = s.find_all('table')
 
-print(tables[0])
+# 첫번째 테이블은 버린다.
+tables = s.find_all('table')[1:]
+
+for table in tables:
+	trlen = len(table.find_all('tr'))
+	if trlen > 1:
+		print(table.tr.td.span.string)
