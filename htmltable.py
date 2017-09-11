b1c754eea9116a0f92afff7f067bc43ccbab7928
vincent <nattybear@naver.com>
2017-09-11 21:24:59 +0900

    테이블 파싱 코드 추가
    행 개수가 2 이상인 테이블만 작업을 한다. 태그 안의 문자열만 모아서 리스트로 만든다. 범용 html 테이블 파서를 제작하려고 했는데 우선 해당 작업은 중지한다. 일단은 브랜치를 하나 따서 ISDR에 대한 작업을 해보려고 한다.

--- htmltable.py
+++ htmltable.py
@@ -2,6 +2,7 @@
 
 from sys import argv
 from bs4 import BeautifulSoup
+from sqlite3 import connect
 
 # ISAC 파일 인코딩은 UTF-16 리틀 엔디안이다.
 f = open(argv[1], encoding='utf-16-le')
@@ -14,7 +15,29 @@ s = BeautifulSoup(b, 'html.parser')
 # 첫번째 테이블은 버린다.
 tables = s.find_all('table')[1:]
 
+lists = []
 for table in tables:
 	trlen = len(table.find_all('tr'))
+	rows = []
+	# 행의 개수가 2개 이상인 테이블만 작업한다.
 	if trlen > 1:
-		print(table.tr.td.span.string)
+		for tr in table.find_all('tr'):
+			col = []
+			for td in tr.find_all('td'):
+				col.append(td.span.string)
+			# 칼럼을 모두 모아서 한 열로 저장한다.
+			rows.append(col)
+	# 행을 모두 모아서 하나의 테이블로 저장한다.
+	lists.append(rows)
+
+# 사전을 정의한다.
+# 행 첫째 줄의 항목을 검사해서
+# 어느 테이블에 입력할지 결정한다.
+# 아래 없는 항목은 따로 조건문을 이용한다.
+dic = {
+	'탐지건수'	: '요주의 IP 탐지현황',
+	'공격 IP'	: '전자적 침해시도 주요 내역',
+	'대상'		: '피싱/파밍사이트 내역' }
+
+for table in lists:
+	
